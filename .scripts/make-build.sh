1e9646c07e482553350181dfa4d51d54916216be
s.ormazabal <s.ormazabal@saltosystems.com>
2025-02-18 10:20:23 +0100

    Adding /bin to the main

--- .scripts/make-build.sh
+++ .scripts/make-build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-GOOS=linux GOARCH=amd64 go build -o bin/app-amd64-linux cmd/general-service/main.go   
+GOOS=linux GOARCH=amd64 go build -o bin/app-amd64-linux cmd/general-service/main.go  
+mv  bin/app-amd64-linux bin/general-service
